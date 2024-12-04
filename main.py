d1a5ba98c275dda8af791f2651ccae2403dc6351
lidialazaro <lidialazarorivada7@gmail.com>
2024-12-04 17:10:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,25 @@ if __name__ == "__main__":
     crear_gui(generar_puntos_koch, calcular_perimetro_koch)
 
     # Instrucción para ejecutar: python main.py
+#==========================================================================================
+#CONJUNTO DE MANDELBROT
 
+from physics import generar_conjunto_mandelbrot, contar_puntos_dentro
+from gui import crear_gui
+
+if __name__ == "__main__":
+    # Parámetros de entrada (definidos aquí)
+    plano = (-2.0, 1.0, -1.5, 1.5)
+    resolucion = (500, 500)
+    max_iteraciones = 100
+
+    # Generar el conjunto de Mandelbrot
+    mandelbrot_matrix = generar_conjunto_mandelbrot(plano, resolucion, max_iteraciones)
+
+    # Contar los puntos dentro del conjunto
+    puntos_dentro = contar_puntos_dentro(mandelbrot_matrix, max_iteraciones)
+    print(f"Puntos dentro del conjunto: {puntos_dentro}")
+
+    crear_gui(generar_conjunto_mandelbrot, contar_puntos_dentro)
 
 
