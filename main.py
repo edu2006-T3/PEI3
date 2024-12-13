0e94a55b9ed102a9e13584a3ecc6e033e7ef315a
edu2006-T3 <eduardoGOOL2006@gmail.com>
2024-12-13 11:54:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
-from physics import generar_puntos_sierpinski, calcular_area_sierpinski
-from gui import crear_gui
+from physics import generar_puntos_sierpinski, calcular_area_sierpinski, generar_puntos_koch, calcular_perimetro_koch
+from gui import pantalla_principal, gui_triangulo_sierpinski,
+from numpy import array
+from math import sqrt
 
-if __name__ == "__main__":
-    crear_gui(generar_puntos_sierpinski, calcular_area_sierpinski)
 
+# VALORES INICIALES TRIÁNGULOS DE SIERPINSKIL 
+iteraciones = 0
+longitud_lado = 1.0
+color_triangulo = 'blue'
+    
+# GENERACIÓN DEL TRIÁNGULO INICIAL 
+altura = (sqrt(3) / 2) * longitud_lado
+A = array([0, 0])
+B = array([longitud_lado, 0])                     # Coordenadas de los puntos A, B, y C y altura del triángulo
+C = array([longitud_lado / 2, altura])
 
-# Para ejecutar la gui escribir "python main.py" en la terminal
+# ======================================================================================================================
+
+# Parámetros de entrada - CONJUNTO DE MANDELBROT
+    plano = (-2.0, 1.0, -1.5, 1.5)
+    resolucion = (500, 500)
+    max_iteraciones = 100
+
+# ======================================================================================================================
+
+pantalla_principal (generar_puntos_sierpinski, calcular_area_sierpinski, iteraciones, longitud_lado, color_triangulo, A, B, C)
+
+# Dar a correr
 
 
 #=======================================================================================
