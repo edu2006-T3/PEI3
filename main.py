7531eeb82bd847b1f7e38f45ebb1440917502a88
edu2006-T3 <eduardoGOOL2006@gmail.com>
2024-12-13 13:37:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from physics import generar_puntos_sierpinski, calcular_area_sierpinski, generar_puntos_koch, calcular_perimetro_koch
-from gui import pantalla_principal, gui_triangulo_sierpinski,
+from physics import generar_puntos_sierpinski, calcular_area_sierpinski, contar_puntos_dentro, generar_conjunto_mandelbrot
+from gui import gui_triangulo_sierpinski, pantalla_principal
 from numpy import array
 from math import sqrt
 
+# #=======================================================================================
+# #=======================================================================================
 
-# VALORES INICIALES TRIÁNGULOS DE SIERPINSKIL 
+# Valores iniciales
 iteraciones = 0
 longitud_lado = 1.0
 color_triangulo = 'blue'
@@ -15,16 +17,16 @@ A = array([0, 0])
 B = array([longitud_lado, 0])                     # Coordenadas de los puntos A, B, y C y altura del triángulo
 C = array([longitud_lado / 2, altura])
 
-# ======================================================================================================================
+# #=======================================================================================
 
 # Parámetros de entrada - CONJUNTO DE MANDELBROT
-    plano = (-2.0, 1.0, -1.5, 1.5)
-    resolucion = (800, 800)
-    max_iteraciones = 0
+plano_inicial = (-2.0, 1.0, -1.5, 1.5)
+resolucion_inicial = (800, 800)
+iteraciones_iniciales = 0
 
-# ======================================================================================================================
+# #=======================================================================================
 
-pantalla_principal (generar_puntos_sierpinski, calcular_area_sierpinski, iteraciones, longitud_lado, color_triangulo, A, B, C)
+pantalla_principal (generar_puntos_sierpinski, calcular_area_sierpinski, iteraciones, longitud_lado, color_triangulo, A, B, C, generar_conjunto_mandelbrot, contar_puntos_dentro, plano_inicial, resolucion_inicial, iteraciones_iniciales)
 
 # Dar a correr
 
