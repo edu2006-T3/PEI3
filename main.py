97913d779e90a1bd93cbca3ebdd476cf48d647cd
lidialazaro <lidialazarorivada7@gmail.com>
2024-12-18 17:28:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,63 @@
-from physics import generar_puntos_sierpinski, calcular_area_sierpinski, contar_puntos_dentro, generar_conjunto_mandelbrot, generate_koch_snowflake,generate_koch_snowflake, calculate_perimeter_koch, calculate_area_koch
+from physics import generar_puntos_sierpinski, calcular_area_sierpinski, generar_conjunto_mandelbrot, contar_puntos_dentro, generar_segmentos_koch, calcular_longitud_koch
 from gui import pantalla_principal
 from numpy import array
 from math import sqrt
 
-# #=======================================================================================
+
 # #=======================================================================================
 
-# Valores iniciales
+
+# Valores iniciales sierpinski
+
+
 N = 0                                 # número de iteraciones iniciales
 l = 1.0                               # longitud del lado inicial del triángulo
 color = 'blue'
-    
-# GENERACIÓN DEL TRIÁNGULO INICIAL 
+   
+# GENERACIÓN DEL TRIÁNGULO INICIAL
+
+
 h = (sqrt(3) / 2) * l                 # altura
 A = array([0, 0])
 B = array([l, 0])                     # Coordenadas de los puntos A, B, y C y altura del triángulo
 C = array([l / 2, h])
 
+
 # #=======================================================================================
 
+
+# Valores iniciales Koch
+
+
+iteraciones_koch = 0                  # número de iteraciones iniciales
+longitud_inicial = 1.0                # longitud del segmento inicial
+color_koch = 'red'                    # color inicial del fractal de Koch
+
+
+# Coordenadas iniciales de los extremos del segmento
+extremo1 = array([0, 0])
+extremo2 = array([longitud_inicial, 0])
+
+
+# #=======================================================================================
+
+
 # Parámetros de entrada - CONJUNTO DE MANDELBROT
+
+
 plano_inicial = (-2.0, 1.0, -1.5, 1.5)
 resolucion_inicial = (800, 800)
 iteraciones_iniciales = 0
 
-# #=======================================================================================
-
-# VALORES INICIALES DEL COPO DE NIEVE DE KOCH
-
-iteraciones = 0  # Número de iteraciones (puede ser modificado desde la GUI)
-escala = 1.0  # Tamaño del fractal
-color_fractal = 'blue'  # Color del fractal
-
-# GENERACIÓN DEL TRIÁNGULO BASE
-# Coordenadas iniciales del triángulo equilátero que define la base del copo
-altura = (sqrt(3) / 2) * escala
-A1 = array([0, 0])
-B1 = array([escala, 0])  # Lado horizontal del triángulo
-C1 = array([escala / 2, altura])  # Punto superior
 
 # #=======================================================================================
 
-pantalla_principal(generar_puntos_sierpinski, calcular_area_sierpinski, N, l, color, A, B, C, generar_conjunto_mandelbrot, contar_puntos_dentro, plano_inicial, resolucion_inicial, iteraciones_iniciales,generate_koch_snowflake, calculate_area_koch, calculate_perimeter_koch)
+
+pantalla_principal(
+    generar_puntos_sierpinski, calcular_area_sierpinski, N, l, color, A, B, C, 
+    generar_conjunto_mandelbrot, contar_puntos_dentro, plano_inicial, resolucion_inicial, iteraciones_iniciales,  
+    generar_segmentos_koch, calcular_longitud_koch, iteraciones_koch, longitud_inicial, color_koch, extremo1, extremo2
+)
 
 
 
